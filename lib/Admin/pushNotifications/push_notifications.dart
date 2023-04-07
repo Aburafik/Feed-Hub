c24318b0aae686c27f554cab947e8f271b4689a3
Rafik <rfkabubakari@gmail.com>
2023-04-07 15:54:03 +0000

    Fixed mino errors

--- lib/Admin/pushNotifications/push_notifications.dart
+++ lib/Admin/pushNotifications/push_notifications.dart
@@ -24,43 +24,49 @@ class PushNotifications extends StatelessWidget {
             child: const Text("Send BroadCast Message"),
           ),
         ),
-        Card(
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FormFieldComponent(
-                    controller: messageTitleController,
-                    label: "Message Title",
-                    errorMessage: "Title",
+        Center(
+          child: Container(
+            height: 300,
+            width: 500,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FormFieldComponent(
+                        controller: messageTitleController,
+                        label: "Message Title",
+                        errorMessage: "Title",
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        child: FormFieldComponent(
+                          controller: messageBodyController,
+                          label: "Message Body",
+                          maxLines: 2,
+                          errorMessage: "Message body",
+                        ),
+                      ),
+                      CommonButton(
+                        buttonText: "Send Message",
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            await donationServices.sendPushNotication(
+                                title: messageTitleController.text.toString(),
+                                body: messageBodyController.text.toString(),context: context);
+                            await AuthUser.sendPushMessage(
+                              title: messageTitleController.text.toString(),
+                              message: messageBodyController.text.toString(),
+                            );
+                          }
+                        },
+                      ),
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    child: FormFieldComponent(
-                      controller: messageBodyController,
-                      label: "Message Body",
-                      maxLines: 2,
-                      errorMessage: "Message body",
-                    ),
-                  ),
-                  CommonButton(
-                    buttonText: "Send Message",
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        await donationServices.sendPushNotication(
-                            title: messageTitleController.text.toString(),
-                            body: messageBodyController.text.toString(),context: context);
-                        await AuthUser.sendPushMessage(
-                          title: messageTitleController.text.toString(),
-                          message: messageBodyController.text.toString(),
-                        );
-                      }
-                    },
-                  ),
-                ],
+                ),
               ),
             ),
           ),
